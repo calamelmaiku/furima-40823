4991c659eb6984f3e3308855f0201d1dc2751d94
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-07 20:54:28 +0900

    testコード(空では登録できない)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,47 @@ RSpec.describe User, type: :model do
   end
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
+      it "nicknameとemail、passwordとpassword_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
     end
     context '新規登録できないとき' do
-      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
-      end
       it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
       it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it 'last_name_kanaが空では登録できない' do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it 'first_name_kanaが空では登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it 'birthが空では登録できない' do
       end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
       end
@@ -27,6 +59,22 @@ RSpec.describe User, type: :model do
       it 'passwordが5文字以下では登録できない' do
       end
       it 'passwordが129文字以上では登録できない' do
+      end
+      it 'passwordが英字のみでは登録できない' do
+      end
+      it 'passwordが数字のみでは登録できない' do
+      end
+      it 'last_nameが半角では登録できない' do
+      end
+      it 'first_nameが半角では登録できない' do
+      end
+      it 'last_name_kanaが全角(漢字)では登録できない' do
+      end
+      it 'first_name_kanaが全角(漢字)では登録できない' do
+      end
+      it 'last_name_kanaが全角(ひらがな)では登録できない' do
+      end
+      it 'first_name_kanaが全角(ひらがな)では登録できない' do
       end
     end
   end
