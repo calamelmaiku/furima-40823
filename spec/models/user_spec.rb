3eaed666d4f998a0a7c4025b2ef6f2c07bbd0e7e
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-07 21:21:43 +0900

    testコード(email)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,8 +57,15 @@ RSpec.describe User, type: :model do
       it 'nicknameが7文字以上では登録できない' do
       end
       it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'passwordが5文字以下では登録できない' do
         @user.password = '12q45'
