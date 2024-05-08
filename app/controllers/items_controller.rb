730bbcceb20904b94d81e531d23752d486594f32
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-08 18:25:50 +0900

    アクティブハッシュプルダウン(category)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :comment, :category_id, :condition_id, :delivery_id, :prefecture_id, :span_id, :price, :user )
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one    :order
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  #空の投稿を保存できないようにする
+  validates :name, :text, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
 end
