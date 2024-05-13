0327f65e527bebc6be7d7e48e6b59e6dbb02bf43
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-13 18:12:14 +0900

    testコード作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,6 +9,57 @@ RSpec.describe Item, type: :model do
       it "nameとcomment、category_idとcondition_id、delivery_id、prefecture_id、span_id、price、userが存在すれば登録できる" do
         expect(@item).to be_valid
       end
+      it 'imageが空でも登録できる' do
+        @item.image = nil
+        expect(@item).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'commentが空では登録できない' do
+        @item.comment = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Comment can't be blank")
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category is not a number")
+      end
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition is not a number")
+      end
+      it 'delivery_idが空では登録できない' do
+        @item.delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery is not a number")
+      end
+      it 'prefecture_idが空では登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+      end
+      it 'span_idが空では登録できない' do
+        @item.span_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Span is not a number")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'userが紐付いていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
