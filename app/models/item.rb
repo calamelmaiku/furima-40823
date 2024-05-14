66559c0ac848a54320df10a878e8501471fdcc41
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-14 14:58:29 +0900

    修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,10 @@ class Item < ApplicationRecord
   belongs_to :span
 
   validates :name, presence: true
+  validates :name, presence: true
   validates :comment, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,10 +9,6 @@ RSpec.describe Item, type: :model do
       it "nameとcomment、category_idとcondition_id、delivery_id、prefecture_id、span_id、price、userが存在すれば登録できる" do
         expect(@item).to be_valid
       end
-      it 'imageが空でも登録できる' do
-        @item.image = nil
-        expect(@item).to be_valid
-      end
     end
     context '新規登録できないとき' do
       it 'nameが空では登録できない' do
@@ -69,6 +65,11 @@ RSpec.describe Item, type: :model do
         @item.price = '１０００'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'userが紐付いていないと登録できない' do
         @item.user = nil
