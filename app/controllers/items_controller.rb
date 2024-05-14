26a1cc0b8f9382be9cda0830b9b407780c812afa
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-14 20:46:26 +0900

    可読性向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :get_params_id, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -43,11 +41,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :comment, :category_id, :condition_id, :delivery_id, :prefecture_id, :span_id, :price).merge(user_id: current_user.id)
   end
 
+  def get_params_id
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-    unless current_user.id == Item.find(params[:id]).user_id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
