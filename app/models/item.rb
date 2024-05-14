f9b990e3b919bc48d23529689f109f85e1dcc73e
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-14 15:20:07 +0900

    修正3

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :name, presence: true
   validates :comment, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
