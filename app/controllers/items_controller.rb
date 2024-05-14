ab9cf9a822abd6da88eb098191913f37cb84516a
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-14 20:21:25 +0900

    Revert "アップデートアクション定義"
    This reverts commit 1ad0c50ec4f4f0b2dcacce59a2f6157e2f36f439.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,13 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to item_path
-    else
-      render :edit, status: :unprocessable_entity
-    end
   end
 
   private
