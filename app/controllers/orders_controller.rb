be1cfd766cf15fabb308fadba578df39129784dd
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-15 18:18:04 +0900

    コントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,24 @@
 class OrdersController < ApplicationController
   def index
+    @address_order = AddressOrder.new
+  end
+
+  def new
+  end
+
+  def create
+    @address_order = AddressOrder.new(donation_params)
+    if @donation_address.valid?
+      @donation_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:address_order).permit(:postcode, :prefecture_id, :city, :street, :build, :telephone, :order_id).merge(user_id: current_user.id)
   end
 end
