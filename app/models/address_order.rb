41690ac143bc3f403ec62250de51404e71f94a1d
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-15 17:24:55 +0900

    Formオブジェクト作成

--- app/models/address_order.rb
+++ app/models/address_order.rb
@@ -1,10 +1,19 @@
 class AddressOrder
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :street, :build, :telephone, :order
+  attr_accessor :postcode, :prefecture_id, :city, :street, :build, :telephone, :order_id
 
-  # ここにバリデーションの処理を書く
+  with_options presence: true do
+
+    validates :postalcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :street
+    validates :telephone
+    validates :order_id
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, street: street, build: build, telephone: telephone)
   end
 end
