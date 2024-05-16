fcabea25ee26ead9b670a396cdc91bb5753965b0
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-16 16:55:05 +0900

    テストコード準備

---
+++ spec/factories/address_orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :address_order do
+    postcode       {'000-0000'}
+    prefecture_id  {Faker::Number.within(range: 1..47)}
+    city           {Faker::Address.city}
+    street         {Faker::Address.street_name}
+    build          {Faker::Address.street_address}
+    telephone      {0000000000}
+  end
+end
---
+++ spec/models/address_order_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe AddressOrder, type: :model do
+  before do
+    item = FactoryBot.build(:item)
+    user = FactoryBot.build(:user)
+    @address_order = FactoryBot.build(:address_order, item_id: item.id, user_id: user.id)
+  end
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it "postcodeとprefecture_id、cityとstreet、telephone、orderが存在すれば登録できる" do
+        expect(@address_order).to be_valid
+      end
+      it "buildが空でも登録できる" do
+        @address_order.build = ''
+        expect(@address_order).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it 'postcodeが空では登録できない' do
+        @address_order.postcode = ''
+        @address_order.valid?
+        expect(@address_order.errors.full_messages).to include("Postcode can't be blank")
+      end
+    end
+  end
+end
