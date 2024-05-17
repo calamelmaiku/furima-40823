f7223cc6bce11f193bbdcf0c87f334b484703b2c
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-17 15:56:43 +0900

    出品者以外しか遷移できない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :get_params_id, only: [:index, :update]
+  before_action :move_to_index, only: [:index]
+
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @address_order = AddressOrder.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -19,6 +21,16 @@ class OrdersController < ApplicationController
 
   private
 
+  def get_params_id
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    unless current_user.id != @item.user_id
+      redirect_to controller: 'items', action: 'index'
+    end
+  end
+
   def address_params
     params.require(:address_order).permit(:postcode, :prefecture_id, :city, :street, :build, :telephone, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
@@ -26,7 +38,7 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: Item.find(params[:item_id]).price, # 商品の値段
+      amount: @item.price, # 商品の値段
       card: address_params[:token], # カードトークン
       currency: 'jpy' # 通貨の種類（日本円）
     )
